74832647ce5535fbead749afa3bf586260e7f175
terN1X <90840218+frelss@users.noreply.github.com>
2024-08-18 11:39:31 +0200

    Time: 14 ms (34.37%), Space: 44.8 MB (29.93%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s.isEmpty()) return false;
+        String cleanedSentence = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+        
+        int left = 0;
+        int right = cleanedSentence.length() - 1;
+        
+        while(left < right){
+            char first = cleanedSentence.charAt(left);
+            char last = cleanedSentence.charAt(right);
+            
+            if(first != last) return false;
+            left++;
+            right--;
+        }
+        
+        return true;
+            
+        
+     /* 
+        String cleanedSentence = s.toLowerCase().replaceAll("[^a-zA-Z]", "");
+
+        StringBuilder result = new StringBuilder();
+        for (char c : cleanedSentence.toCharArray()) {
+            if (c != ' ') {
+                result.append(c);
+            }
+        }
+
+        String original = result.toString();
+        String reversed = result.reverse().toString();
+
+        return original.equals(reversed); 
+        */
+    }
+}
